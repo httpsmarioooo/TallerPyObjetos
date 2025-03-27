2ba94bfa2bbf42bf298b4e7affd843b4bfd1420e
Carlos Mario <carlosmariobig14@gmail.com>
2025-03-27 17:14:23 -0500

    task 4 actualizado pero fue algo muy x

--- ejercicios/Task4.js
+++ ejercicios/Task4.js
@@ -2,17 +2,16 @@
 function Termometro() {
     this.actualizar = function (grados) {
         if (grados > 30) {
-            console.log("¡Hace calor!");
+            console.log("¡Hace calor ahhhhhh!");
         } else if (grados < 10) {
-            console.log("¡Hace frío!");
+            console.log("¡Hace fi fi fi fri frío!");
         } else {
             console.log("Temperatura agradable");
         }
     };
 }
-
 // 3. Crear una instancia de Termómetro
 let termometro = new Termometro();
-termometro.actualizar(5);   // ¡Hace frío!
-termometro.actualizar(20);  // Temperatura agradable
-termometro.actualizar(35);  // ¡Hace calor!
+termometro.actualizar(5);   
+termometro.actualizar(20);  
+termometro.actualizar(35);  
